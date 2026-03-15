6cfe8fa37f9b380cd5d6908456c0f4270b78fa26
D.Raju <d.raju@tudelft.nl>
2026-03-15 02:46:36 +0100

    Update BINARY_INTERACTION_PARAMETERS submodule pointer

--- GIT.sh
+++ GIT.sh
@@ -2,6 +2,6 @@
 
 # git init
 git add *
-git commit -m "Added GIT.sh"
-git remote add origin https://github.com/Darz2/thermoift.git
+git commit -m "Update BINARY_INTERACTION_PARAMETERS submodule pointer"
+# git remote add origin https://github.com/Darz2/thermoift.git
 git push -u origin master
